4c5cc4638ffebf8d8ddf381a09d8d4c7f1071b0e
adam <financeCoding@gmail.com>
2012-09-30 16:34:35 -0700

    some stub static methods for common in linear algebra, matrix statics should be defined.

--- lib/scalarlistutils.dart
+++ lib/scalarlistutils.dart
@@ -3,3 +3,4 @@
 #source('src/memory.dart');
 #source('src/c_string.dart');
 #source('src/byteorder.dart');
+#source('src/linear_algebra.dart');
---
+++ lib/src/linear_algebra.dart
@@ -0,0 +1,40 @@
+class CholeskyDecomposition {
+  static triangularFactorL(ByteArray matrix) => throw "Not Implemented";
+  static spd(ByteArray matrix) => throw "Not Implemented";
+  static solve(ByteArray a, ByteArray b) => throw "Not Implemented";
+}
+
+class EigenvalueDecomposition {
+  static diagonalEigenValue(ByteArray matrix) => throw "Not Implemented";
+  static imagEigenValues(ByteArray matrix) => throw "Not Implemented";
+  static realEigenValues(ByteArray matrix) => throw "Not Implemented";
+  static eigenVector(ByteArray matrix) => throw "Not Implemented";
+}
+
+class LUDecomposition {
+  static det(ByteArray matrix) => throw "Not Implemented";
+  static doublePivot(ByteArray matrix) => throw "Not Implemented";
+  static lowerTriangularFactor(ByteArray matrix) => throw "Not Implemented";
+  static pivot(ByteArray matrix) => throw "Not Implemented";
+  static upperTriangularFactor(ByteArray matrix) => throw "Not Implemented";
+  static bool nonSingular(ByteArray matrix) => throw "Not Implemented";
+  static solve(ByteArray a, ByteArray b) => throw "Not Implemented";
+}
+
+class QRDecomposition {
+  static houseHolderVectors(ByteArray matrix) => throw "Not Implemented";
+  static orthogonalFactor(ByteArray matrix) => throw "Not Implemented";
+  static upperTriangularFactor(ByteArray matrix) => throw "Not Implemented";
+  static bool fullRank(ByteArray matrix) => throw "Not Implemented";
+  static solve(ByteArray a, ByteArray b) => throw "Not Implemented";
+}
+
+class SingularValueDecomposition {
+  static conditionalNumber(ByteArray matrix) => throw "Not Implemented";
+  static diagonalSingularValues(ByteArray matrix) => throw "Not Implemented";
+  static singularValues(ByteArray matrix) => throw "Not Implemented";
+  static leftSingularVectors(ByteArray matrix) => throw "Not Implemented";
+  static rightSingularVectors(ByteArray matrix) => throw "Not Implemented";
+  static norm2(ByteArray matrix) => throw "Not Implemented";
+  static rank(ByteArray matrix) => throw "Not Implemented";
+}
