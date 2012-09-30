c08caca57fe0fd916a054c7b8f31645c914ccb71
John McCutchan <john@johnmccutchan.com>
2012-09-30 13:24:49 -0700

    Implement swapEndian
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/src/byteorder.dart
+++ lib/src/byteorder.dart
@@ -1,7 +1,20 @@
 
 class ByteOrder {
-  static swapEndian(ByteArray array, int offset, int size) {
+  static swapByte(ByteArray array, int i, int j) {
+    int iValue = array.getUint8(i);
+    int jValue = array.getUint8(j);
+    array.setUint8(i, jValue);
+    array.setUint8(j, iValue);
+  }
 
+  static swapEndian(ByteArray array, int offset, int size) {
+    int i = 0;
+    int j = size-1;
+    while (i < j) {
+      swapByte(array, offset+i, offset+j);
+      i++;
+      j--;
+    }
   }
 
   static hostToNetworkByteOrder(ByteArray array, int offset, int size) {
